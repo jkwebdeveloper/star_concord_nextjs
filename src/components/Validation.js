c2a932f0e31d91a7a542c468f6179edb49a30ba8
Jignesh Koshti <jignesh.theappideas@gmail.com>
2024-09-10 18:29:50 +0530

    apply page done

--- src/components/Validation.js
+++ src/components/Validation.js
@@ -1,4 +1,4 @@
-import * as yup from 'yup' 
+import * as yup from 'yup'
 export const contactValidation = {
     initialState: {
         name: "",
@@ -18,4 +18,36 @@ export const contactValidation = {
     }),
 };
 
-
+const ValidationSchema = () => {
+    const careerApplySchema = yup.object().shape({
+        firstname: yup.string().required("This field is required !"),
+        lastname: yup.string().required("This field is required !"),
+        email: yup.string().email().required("This field is required !"),
+        mobile: yup.string().required("Phone number is must be required"),
+        location: yup.string().required("This field is required !"),
+        jobId: yup.string().required("This field is required !"),
+        resume: yup
+            .mixed()
+            .required("resume is required")
+            .test("fileSize", "File should be less than 2 MB!!!", (value) => {
+                return value && value[0].size <= 2_000_000;
+            })
+            .test(
+                "type",
+                "Only the following formats are accepted: .pdf, .doc, .docx",
+                (value) => {
+                    return (
+                        value &&
+                        (value[0].type === "application/pdf" ||
+                            value[0].type === "application/msword" ||
+                            value[0].type ===
+                            "application/vnd.openxmlformats-officedocument.wordprocessingml.document")
+                    );
+                }
+            ),
+    })
+    return {
+        careerApplySchema,
+    };
+}
+export default ValidationSchema;
